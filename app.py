5aaf26cee4d4c79e24908088fe05618ecb3c8c65
chima <chimaataman@gmail.com>
2020-07-22 18:43:19 +0100

    hook base url to 404.html; change swagger ui route to /v1/doc

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from settings.config import connex_app
-from flask import redirect
+from flask import render_template, url_for
 
-connex_app.add_api ('swagger.yaml', arguments={'swagger_url': 'doc'})
+connex_app.add_api('swagger.yaml', options={"swagger_url": '/doc'})
 application = connex_app.app
 
 
 @connex_app.route('/')
 def redir():
-    return redirect('/v1/ui', 302)
+    swagger_doc_url = 'v1/doc'
+    return render_template('404.html', url_for=url_for, doc=swagger_doc_url)
 
 
 if __name__ == '__main__':
