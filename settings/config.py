b1ac6828a4348f048a3fa8e8f58064475430eba5
chima <chimaataman@gmail.com>
2020-07-22 20:24:25 +0100

    add db migration commands

--- settings/config.py
+++ settings/config.py
@@ -4,6 +4,7 @@ import connexion
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 
 base_dir = os.path.dirname(__file__)
 db_path = os.path.join(base_dir, 'database')
@@ -20,5 +21,6 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 tokens = {}
