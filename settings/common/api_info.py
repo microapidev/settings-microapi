ebd1983855e230cbeb4098dcea1fe18cc59403b1
chima <chimaataman@gmail.com>
2020-08-04 02:30:16 +0100

    add favicon

--- settings/common/api_info.py
+++ settings/common/api_info.py
@@ -1,11 +1,13 @@
-# imports go here
-
-api_info = {
-    "title": "Settings MicroApi",
-    "description": "Finally a place to store and dynamically retrieve configurations for your countless apps and services"
-}
+from flask import request, redirect, url_for
 
 
 def get():
+    url_root = request.url_root
+    url_root = url_root[:-1] if url_root[-1] == '/' else url_root
+    api_info = {
+        "title": "Settings MicroApi",
+        "description": "Finally a place to store and dynamically retrieve configurations for your countless apps and services",
+        "icon": url_root + url_for('static', filename='img/favicon.ico'),
+    }
     return api_info
 
